29a73791ddad0c1e345dbd8aaf8e290bd915c410
KellisonSilva <shellsCode@protonmail.com>
2022-01-11 21:05:32 -0400

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,47 @@
-import Navbar from "./componets/Navbar";
+// import Navbar from "./componets/Navbar";
+// //componente react que vai ser o nav(funcao do javaScript)
+// function App() {
+//   //retorna todo o codigo parecido html porem eh um formato especifico chamada JSX.. entrada de codigo do react
+//   return (
+//    <Navbar/>
+//   );
+// }
+// //exportanto a funcao app para arquivo principal (index.tsx)
+// export default App;
+// importanto três componentes do router-dom responsavel por configurar as rotas.
+// importar também os componentes para ativa-los em cada rota. 
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
-//componente react que vai ser o nav(funcao do javaScript)
 function App() {
-  //retorna todo o codigo parecido html porem eh um formato especifico chamada JSX.. entrada de codigo do react
+  // browserRouterInicia a rota
+  // dentro dele o navbar
+  // routes responsavel por criar cada rota.
+
+    //1P router abrir a pagina listing
+    // localhost/
+    
+    //2P router caminho localhost/3000/form
+    //existem uma subrota para /form que recebe um parametro que vai ser o id do filme
+    //localhost/3000/form/1....
+
   return (
-   <Navbar/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
-//exportanto a funcao app para arquivo principal (index.tsx)
+
 export default App;
---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,7 @@
+function Form() {
+    return (
+        <h1>Página do formúlario</h1>
+    );
+}
+
+export default Form;
