da499c5464970ff6cad675f0f4498842a4fa0dc6
KellisonSilva <shellsCode@protonmail.com>
2022-01-11 22:35:37 -0400

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,33 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 function Listing() {
-    return(
+    return (
+        <> <Pagination />
 
-        <Pagination/ >
+            <div className="container">
+                {/*a imagem estava tomando toda a tela e com a utilizacao da classe row do bootstrap, ela organiza os elementos*/}
+                {/*Estilo do bootstrap margin button == mg que irá acrescentar uma margem abaixo do card*/}
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mg-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mg-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mg-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mg-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mg-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
+
     );
 }
 
