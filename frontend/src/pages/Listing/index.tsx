5f19dc5b331e459d3943da3632ab073f87220631
KellisonSilva <shellsCode@protonmail.com>
2022-01-15 08:27:48 -0400

    atualizando paginacao por titulo

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -26,7 +26,7 @@ function Listing() {
 
     //Ordenar os filmes pelo id ou pelo titulo alterando a &sort=title
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
             .then(response => {
                 const data = response.data as MoviePage;
                 setPage(data);
