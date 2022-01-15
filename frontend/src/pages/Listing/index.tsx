204350c0016565b112f3281fad8ad97ce680070d
KellisonSilva <shellsCode@protonmail.com>
2022-01-14 20:41:15 -0400

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -8,43 +8,55 @@ import { BASE_URL } from "utils/requests";
 function Listing() {
 
     const [pageNumber, setPageNumer] = useState(0);
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    });
 
+
+
+    //Ordenar os filmes pelo id ou pelo titulo alterando a &sort=id
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
             .then(response => {
                 const data = response.data as MoviePage;
-                console.log(data);
-                setPageNumer(data.number);
-
+                setPage(data);
             });
 
-    }, []);
+    }, [pageNumber]);
+
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
 
     return (
         <>
-            <p>{pageNumber}</p>
-
             <Pagination />
 
             <div className="container">
                 {/*a imagem estava tomando toda a tela e com a utilizacao da classe row do bootstrap, ela organiza os elementos*/}
                 {/*Estilo do bootstrap margin button == mg que irá acrescentar uma margem abaixo do card*/}
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mg-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mg-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mg-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mg-3">
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mg-3">
-                        <MovieCard />
-                    </div>
+
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mg-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    )
+                    )}
+
                 </div>
             </div>
         </>
